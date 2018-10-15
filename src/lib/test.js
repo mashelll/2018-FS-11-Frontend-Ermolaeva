42b4ddcc395acdec25dc698830ebb851841639a0
masha <biruza2255@mail.ru>
2018-10-15 13:23:53 +0300

    Added eslint run script, removed useless files

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,6 +1,6 @@
-const say = function (){
-  var myTable= "<table><tr><td style='width: 200px; height: 100px; background-color: LightCyan; text-align: center;'> Hello, world!</td>";
-  document.write( myTable);
+function say() {
+  const myTable = "<table><tr><td style='width: 200px; height: 100px; background-color: LightCyan; text-align: center;'> Hello, world!</td>";
+  document.write(myTable);
 }
 
 export default say;
