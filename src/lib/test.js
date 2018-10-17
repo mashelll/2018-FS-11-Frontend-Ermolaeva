ae900e49c309b7da7268dbe44493badae43ccac8
masha <biruza2255@mail.ru>
2018-10-17 21:36:00 +0300

    Finished the eslint script

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,5 +1,5 @@
 function say(name) {
-  //const myTable = "<table><tr><td style='width: 200px; height: 100px; background-color: LightCyan; text-align: center;'> Hello, world!</td>";
+  // const myTable = "<table><tr><td style='width: 200px; height: 100px; background-color: LightCyan; text-align: center;'> Hello, world!</td>";
   document.write(`Hello, ${name}!`);
 }
 
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+/*const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  }
+};*/
+
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'mini-css-extract-plugin',
+            options: {
+              publicPath: '../'
+            }
+          },
+          "css-loader"
+        ]
+       
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'style.css' }),
+  ],
+};
+
+
