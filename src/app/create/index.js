acaad32a757d679ed5fbb848417e789a4d0bda61
masha <biruza2255@mail.ru>
2018-11-07 23:13:07 +0300

    Added function getReadableSize in index.html and was an attempt to improve send_file button

--- src/app/create/index.js
+++ src/app/create/index.js
@@ -1,4 +1,2 @@
 import FormInput from '../../lib/components/form/-input';
 import MessageForm from '../../lib/components/message-form';
-import getReadableSize from '../../utils/utils.js';
-alert('10000='+getReadableSize(10000));
